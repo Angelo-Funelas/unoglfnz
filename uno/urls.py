2986436f8cafbaf2bf7a6fcd1928ef568f8b4c78
git push <martinangelom.funelas.10@gmail.com>
2021-05-25 14:03:36 +0800

    Updated to v1.0.1

--- uno/urls.py
+++ uno/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("server", views.server, name="server"),
-    path("joinroom", views.joinroom, name="joinroom"),
-    path("create/<str:name>", views.create, name="create"),
-    path("game", views.game, name="game")
+    path("game", views.game, name="game"),
+    path("dev", views.dev, name="development")
 ]
